0967c222da9c0f6f03534de11b0cf87625b4f1b4
Jonathan Diehl <jonathan.diehl@gmail.com>
2012-07-11 15:46:03 +0200

    resolve variables

--- Breakpoint.js
+++ Breakpoint.js
@@ -108,9 +108,27 @@ define(function (require, exports, module) {
 
 		// add trace information
 		addTrace: function (res) {
-			if (this.trace) {
-				this.trace.push({ date: new Date().getTime(), callFrames: res.callFrames });
-				$(this).triggerHandler("trace", this);
+			if (!this.trace) return;
+
+			function onResolve(res) {
+				// for now, only save primitive values
+				this.vars = {};
+				for (var i in res.result) {
+					var info = res.result[i];
+					this.vars[info.name] = info.value.value;
+				}
+				console.log(res.result);
+			}
+
+			var callFrames = res.callFrames;
+			this.trace.push({ date: new Date().getTime(), callFrames: callFrames });
+			$(this).triggerHandler("trace", this);
+
+			// resolve call frame scope variables
+			var cf = callFrames[0];
+			if (!cf || !cf) return;
+			for (var i in cf.scopeChain) {
+				Inspector.Runtime.getProperties(cf.scopeChain[i].object.objectId, true, onResolve.bind(cf.scopeChain[i]));
 			}
 		},
 
