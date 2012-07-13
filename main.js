e0046526949099bcb340e55783ac8286102d4301
Dennis Kehrig <mailgithub@denniskehrig.de>
2012-07-13 17:22:46 +0200

    When hovering over variable, find the surrounding function

--- main.js
+++ main.js
@@ -82,32 +82,6 @@ define(function (require, exports, module) {
 		});
 	}
 
-	// Taken from LiveDevelopment.js - this should REALLY be somewhere else
-    /** Augments the given Brackets document with information that's useful for live development. */
-    function _setDocInfo(doc) {
-        // FUTURE: some of these things should just be moved into core Document; others should
-        // be in a LiveDevelopment-specific object attached to the doc.
-        var matches = /^(.*\/)(.+\.([^.]+))$/.exec(doc.file.fullPath);
-        if (matches) {
-            var prefix = "file://";
-            
-            // The file.fullPath on Windows starts with a drive letter ("C:").
-            // In order to make it a valid file: URL we need to add an
-            // additional slash to the prefix.
-            if (brackets.platform === "win") {
-                prefix += "/";
-            }
-            
-            doc.extension = matches[3];
-            doc.url = encodeURI(prefix + doc.file.fullPath);
-
-            // the root represents the document that should be displayed in the browser
-            // for live development (the file for HTML files, index.html for others)
-            var fileName = /^html?$/.test(matches[3]) ? matches[2] : "index.html";
-            doc.root = {url: encodeURI(prefix + matches[1] + fileName)};
-        }
-    }
-
     /** Sets a line class and removes it after a delay */
 	function setTemporaryLineClass(editor, line, klass, delay) {
 		// Make sure no other line class or previous trace class is in the way
@@ -157,6 +131,7 @@ define(function (require, exports, module) {
 			$.each(tracepointsForUrl[url], function (index, tracepoint) {
 				tracepoint.remove();
 			});
+			delete tracepointsForUrl[url];
 		}
 	}
 
@@ -230,9 +205,35 @@ define(function (require, exports, module) {
 		}
 		
 		if (token && token.className === 'variable') {
+			var url = DocumentManager.getCurrentDocument().url;
+			var fns = functionsForUrl[url];
+			if (! fns) { return; }
+
+			console.log("Token", token.start, token.end, "Cursor", cursor.line);
+
+			var closest = null;
+			for (var i = 0; i < fns.length; i++) {
+				var fn = fns[i];
+				console.log("Candidate", fn.loc.start.line + " - " + fn.loc.end.line, fn.id ? fn.id.name : "anonymous");
+				var start = fn.loc.start, end = fn.loc.end;
+
+				var startsBefore = start.line - 1 < cursor.line || (start.line - 1 === cursor.line && start.column < token.start);
+				var endsAfter = end.line - 1 > cursor.line || (end.line - 1 === cursor.line && end.column > token.end);
+
+				console.log(startsBefore, endsAfter, start.line - 1 === cursor.line, start.line, cursor.line, start.column, token.start);
+
+				// Assumption is that any later function that surrounds the variable
+				// is inside any previous surrounding function => just replace closest
+				if (startsBefore && endsAfter) {
+					closest = fn;
+				}
+			}
+			
+			if (! closest) { return; }
+			
 			var pixel = cm.charCoords({ line: cursor.line, ch: token.start }, 'local');
 			console.log("Line", cursor.line, "Column", cursor.ch, "X", pixel.x, "Y", pixel.y, "String", token.string);
-			console.log(pixel);
+			console.log(closest.loc.start.line + " - " + closest.loc.end.line, closest.id ? closest.id.name : "anonymous");
 			$popup = $('<div>')
 				.text(token.string)
 				.css({ left: pixel.x, top: pixel.y, background: 'red', position: 'absolute' });
@@ -324,16 +325,6 @@ define(function (require, exports, module) {
 
 		$(DocumentManager).on("currentDocumentChange", onCurrentDocumentChange);
 		setTimeout(onCurrentDocumentChange, 0);
-
-		// Yes, there is DocumentManager.getAllOpenDocuments
-		// However not all files in the working set are actually "open" for some reason
-		// $.each(DocumentManager.getWorkingSet(), function (index, fileEntry) {
-		// 	if (! fileEntry.isFile || fileEntry.fullPath.replace(/^.*\./, '') !== 'js') { return; }
-		// 	DocumentManager.getDocumentForPath(fileEntry.fullPath).done(function (doc) {
-		// 		_setDocInfo(doc);
-		// 		parseDocument(doc);
-		// 	});
-		// });
 	}
 
 	// unload
