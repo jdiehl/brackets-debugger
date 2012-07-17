5181d88d76fe7cef9bf7eef958800227a4784064
Dennis Kehrig <mailgithub@denniskehrig.de>
2012-07-17 18:34:53 +0200

    Bugfix: consider tokens in different lines as different

--- Hover.js
+++ Hover.js
@@ -209,7 +209,7 @@ define(function (require, exports, module) {
 			cursor.ch   === hover.cursor.ch &&
 			cursor.line === hover.cursor.line
 		) { return; }
-		
+
 		hover.cursor = cursor;
 		onCursorHover(cursor, node, cm);
 	}
@@ -217,13 +217,17 @@ define(function (require, exports, module) {
 	function onCursorHover(cursor, cmLinesNode, cm) {
 		var token = cursor ? cm.getTokenAt(cursor) : null;
 
+		// Tokens don't include line information
+		if (token) { token.line = cursor.line; }
+
 		// Same token hovered as before: abort
 		if (hover.token &&
 			token &&
 			token.string    === hover.token.string &&
 			token.className === hover.token.className &&
 			token.start     === hover.token.start &&
-			token.end       === hover.token.end
+			token.end       === hover.token.end &&
+			token.line      === hover.token.line
 		) { return; }
 
 		hover.token = token;
