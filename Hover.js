ea98f0c3b799ef62a7d61a9559503a76e8b69b15
Dennis Kehrig <mailgithub@denniskehrig.de>
2012-07-27 14:01:48 +0200

    Remove findWrappingFunction, it is now obsolete

--- Hover.js
+++ Hover.js
@@ -215,23 +215,6 @@ define(function (require, exports, module) {
 		return result.promise();
 	}
 
-	function findWrappingFunction(functions, cursor, token) {
-		var fn;
-		
-		for (var i = 0; i < functions.length; i++) {
-			var candidate    = functions[i];
-			var start        = candidate.start, end = candidate.end;
-			var startsBefore = start.line - 1 < cursor.line || (start.line - 1 === cursor.line && start.column < token.start);
-			var endsAfter    = end.line - 1 > cursor.line || (end.line - 1 === cursor.line && end.column > token.end);
-
-			// Assumption: any later function that surrounds the variable
-			// is inside any previous surrounding function => just replace fn
-			if (startsBefore && endsAfter) { fn = candidate; }
-		}
-
-		return fn;
-	}
-
 	/** Event Handlers *******************************************************/
 	
 	function onLinesMouseMove(event) {
